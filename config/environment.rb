6c6c52143a8971dc02782247afc48f90a4a71d1c
Anais NWK <anais.nowak@gmail.com>
2020-05-12 19:11:03 +0200

    Modif clé API

--- config/environment.rb
+++ config/environment.rb
@@ -12,5 +12,5 @@ ActionMailer::Base.smtp_settings = {
 	:address => 'smtp.gmail.com',
 	:port => 587,
 	:authentication => :plain,
-	:enable_starttls_auto => true
+	:enable_starttls_auto => false
 }
