ebf9b97232e4d4c0d7b06e8632fed81926ab0238
Anais NWK <anais.nowak@gmail.com>
2020-05-15 12:08:15 +0200

    update 1505

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
-	before_action :authenticate_user!
-
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
-
+  	@event = Event.all
   end
+
+  
 end
